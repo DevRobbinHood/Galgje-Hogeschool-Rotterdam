1f291e7e8e456823b225ec21105b3a0cc947eb75
Robin <78628698+DevRobbinHood@users.noreply.github.com>
2021-10-26 18:56:14 +0200

    Update galgje.py

--- galgje.py
+++ galgje.py
@@ -1,2 +1,33 @@
-https://www.programiz.com/python-programming/online-compiler hier in maken.
-https://ap.lc/nxgBH  oplossing daarna in vscode zetten.
+print("Galgje spel Tomas de Jonge")
+
+woord = input("Vul een woord in: ")
+print(woord)
+
+spelen = True
+fouten = 0
+woordenlijst = []
+for index in woord: 
+    woordenlijst += "_"
+    
+while spelen: 
+    letter = input("Vul een letter in: ")
+    
+    letterinwoord = letter in woord #True of False 
+    if letterinwoord == False:
+        fouten = fouten + 1
+        print("Je heb een fout gemaakt. Probeer het op nieuw!")
+        
+        if fouten >= 8:
+            print("Je heb verloren game over. Probeer het nog een keer!")
+            spelen = False 
+    
+    else:
+        for index in range(len(woord)):
+            if woord[index] == letter:
+                woordenlijst[index] = letter
+            
+        geraden_woord = ''.join(woordenlijst)
+        if(geraden_woord == woord):
+            print("Gefeliciteerd je heb gewonnen!")
+            spelen = False
+        print(woordenlijst)
